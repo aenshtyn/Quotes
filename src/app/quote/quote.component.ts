afdd6b7e91937bc875765dae8ed70efa31929eb8
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-04-01 13:58:05 +0300

    used the ngSubmit directive to add quotes to the array when submitted

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,7 +13,16 @@ export class QuoteComponent implements OnInit {
     {id:1, name: , author:}
 
     new Quotes(1,'If you are good at something, never do it for free' ,'The Joker', new Date(2012,3,14)),
-  ]
+  ];
+
+  addNewQuote(quote){
+  let quoteLength = this.quote.length;
+  quote.id = goalLength+1;
+  quote.completeDate = new Date(quote.completeDate)
+  quote.upvote = 0
+  quote.downvote = 0
+  this.quotes.push(goal)
+}
 
 }
 
